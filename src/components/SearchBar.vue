5a184f915293826721871471da9fa649c11a9928
Lebarnon <53988015+Lebarnon@users.noreply.github.com>
2023-05-24 02:31:42 +0800

    Made search icon a btn

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="row items-start">
     <q-form
+      ref="form"
       @submit="onSubmit"
       @reset="onReset"
     >
@@ -11,13 +12,15 @@
         filled
         :loading="isLoading"
         placeholder="Search"
-        hint="Try searching SC1005"
+        :hint="isLoading ? 'Searching for a new course code might take awhile' : 'Try searching SC1005'"
+        lazy-rules
+        :rules="[ val => validateCourseCode(val) || 'Please enter a valid course code']"
       >
         <template v-slot:append>
-          <q-icon name="search" />
+          <!-- <q-icon name="search" /> -->
+          <q-btn flat icon="search" @click="form.submit()" />
         </template>
       </q-input>
-      <!-- <q-btn label="Add Course" type="submit" color="primary"/> -->
     </q-form>
   </div>
   <!-- <q-btn @click="timetableStore.testAddCourse()">
@@ -28,7 +31,9 @@
 <script setup>
 import { ref } from 'vue';
 import { useTimetableStore } from '../stores/timetable.js'
+import { validateCourseCode } from '@/composables/validator.js'
 
+const form = ref(null)
 const isLoading = ref(false)
 const timetableStore = useTimetableStore()
 const search = ref(null)
@@ -38,6 +43,7 @@ async function onSubmit(){
   isLoading.value = true;
   await timetableStore.addCourse(search.value) 
   isLoading.value = false
-  search.value=null
+  search.value = null
+  form.value.reset()
 }
 </script>
