7b47de7f183d9a0ad0ddbef8054f6853d01bd70a
Lebarnon <53988015+Lebarnon@users.noreply.github.com>
2023-06-23 14:04:18 +0800

    Refactored layout sections

--- src/App.vue
+++ src/App.vue
@@ -1,51 +1,9 @@
 <template>
-  <q-layout view="hhh LpR fFf">
-
-    <q-header class="bg-primary text-white">
-      <q-toolbar>
-        <!-- <q-btn dense flat round icon="menu" @click="toggleLeftDrawer" /> -->
-
-        <q-toolbar-title class="brand-font">
-          <span class="title" @click="leftDrawerOpen = !leftDrawerOpen">
-            <q-avatar class="q-mb-xs">
-              <img :src="NTUStarsIcon">
-            </q-avatar>
-              NTU Stars
-          </span>
-        </q-toolbar-title>
-        <q-btn flat dense class="q-mr-xs text-capitalize" label="Courses" :to="'/courses'"/>
-        <q-separator color="white" inset spaced dark/>
-        <!-- <q-toggle
-          v-model="leftDrawerOpen"
-          checked-icon="cresent"
-          color="dark"
-          unchecked-icon="dark"
-          @update:model-value="toggleDarkMode"
-        /> -->
-        <q-btn dense flat label="Help" @click="helpDialog = !helpDialog" class="text-capitalize"/>
-        <q-btn dense flat round icon="menu" @click="toggleRightDrawer"  class="q-ml-xs"/>
-      </q-toolbar>
-    </q-header>
-
-    <!-- <q-drawer 
-      show-if-above 
-      v-model="leftDrawerOpen" 
-      side="left" 
-      bordered
-      @show="useTimetableStore().resize()"
-      @hide="useTimetableStore().resize()"
-    >
-      <div v-if="leftDrawerOpen" class="absolute" style="top: 15px; right: -17px">
-        <q-btn
-          dense
-          round
-          unelevated
-          color="primary"
-          icon="chevron_left"
-          @click="leftDrawerOpen = false"
-        />
-      </div>
-    </q-drawer> -->
+  <q-layout view="hHh LpR fff">
+    <HeaderSection 
+      @toggle-right-drawer="rightDrawerOpen = !rightDrawerOpen"
+      @toggle-help-modal="helpDialogOpen = !helpDialogOpen"
+    />
 
     <q-drawer 
       show-if-above 
@@ -58,54 +16,30 @@
       <DrawerStepper />
     </q-drawer>
 
-    <q-dialog v-model="helpDialog">
-      <HelpStepper @handle-finish="helpDialog = false"/>
+    <q-dialog v-model="helpDialogOpen">
+      <HelpStepper @handle-finish="helpDialogOpen = false"/>
     </q-dialog>
 
     <q-page-container>
       <RouterView />
     </q-page-container>
 
-    <q-footer>
-      <q-toolbar class="align-center">
-        <q-toolbar-title shrink class="text-subtitle2">
-          Developed by Lenson
-        </q-toolbar-title>
-        <q-btn stretch color="primary" class="text-capitalize" icon="article" href='https://forms.gle/4CV1ZiXkRS87P8cT9' target="_blank" label=""/>
-        <q-space/>
-        <q-btn stretch color="primary" class="text-capitalize" icon="article" href='https://forms.gle/4CV1ZiXkRS87P8cT9' target="_blank" label="Feedback"/>
-        <q-btn color="primary" class="text-capitalize" icon="bug_report" href='https://forms.gle/4jCSpWpvMUuWLBzh7' target="_blank" label="Bug Report"/>
-      
-        <!-- <q-btn dense flat label="Help" @click="leftDrawerOpen = !leftDrawerOpen" class="text-capitalize"/> -->
-        <!-- <q-btn dense flat round icon="menu" @click="toggleRightDrawer"  class="q-ml-xs"/> -->
-      </q-toolbar>
-    </q-footer>
+    <FooterSection />
 
   </q-layout>
 </template>
 
 <script setup>
-import { ref, nextTick} from 'vue'
+import { ref} from 'vue'
 import { RouterView } from 'vue-router'
-import SearchBar from './components/SearchBar.vue'
-import SelectedList from './components/SelectedList.vue';
 import { useTimetableStore } from './stores/timetable';
-import DrawerStepper from './components/DrawerStepper.vue';
-import { useQuasar } from 'quasar'
-import NTUStarsIcon from '@/assets/NTUStars-icon.png'
+import HeaderSection from './components/layout/HeaderSection.vue';
+import FooterSection from './components/layout/FooterSection.vue';
 import HelpStepper from './components/HelpStepper.vue';
+import DrawerStepper from './components/DrawerStepper.vue';
 
 const rightDrawerOpen = ref(false)
-const leftDrawerOpen = ref(false)
-const $q = useQuasar()
-const darkMode = ref($q.dark.isActive)
-const helpDialog = ref(true)
-function toggleRightDrawer () {
-  rightDrawerOpen.value = !rightDrawerOpen.value
-}
-function toggleDarkMode(){
-  $q.dark.toggle()
-}
+const helpDialogOpen = ref(true)
 
 </script>
 
@@ -113,7 +47,4 @@ function toggleDarkMode(){
 .title{
   cursor: pointer;
 }
-/* .body--light {
-  background-color: #edf2f4;
-} */
 </style>
---
+++ src/components/layout/HeaderSection.vue
@@ -0,0 +1,33 @@
+<template>
+  <q-header class="bg-primary text-white">
+    <q-toolbar>
+      <!-- <q-btn dense flat round icon="menu" @click="toggleLeftDrawer" /> -->
+
+      <q-toolbar-title class="brand-font">
+        <span class="title">
+          <q-avatar class="q-mb-xs">
+            <img :src="NTUStarsIcon">
+          </q-avatar>
+            NTU Stars
+        </span>
+      </q-toolbar-title>
+      <!-- <q-btn stretch dense flat class="q-px-md text-capitalize" label="Courses" :to="'/courses'"/> -->
+      <q-btn stretch dense flat class="q-px-md text-capitalize" label="Courses" @click="$q.notify({message:'Page under construction!'})"/>
+      <q-btn stretch dense flat label="Help" @click="toggleHelpModal" class="text-capitalize q-px-md"/>
+      <q-btn stretch dense flat icon="menu" @click="toggleRightDrawer"  class="q-px-md"/>
+    </q-toolbar>
+  </q-header>
+</template>
+
+<script setup>
+import NTUStarsIcon from '@/assets/NTUStars-icon.png'
+
+const emits = defineEmits(['toggleHelpModal', 'toggelRightDrawer'])
+
+function toggleHelpModal(){
+  emits("toggleHelpModal")
+}
+function toggleRightDrawer(){
+  emits("toggleRightDrawer")
+}
+</script>
