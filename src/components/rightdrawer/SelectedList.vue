ac77f979117474ae865bc49116d51895d5bb7534
Lenson <53988015+Lebarnon@users.noreply.github.com>
2023-07-10 17:15:35 +0800

    Fixed color disappearing bug

--- src/components/rightdrawer/SelectedList.vue
+++ src/components/rightdrawer/SelectedList.vue
@@ -1,14 +1,24 @@
 <template>
   <q-list class="rounded-borders" style="width: 100%;">
     <q-item-label header>Courses</q-item-label>
+    <template v-if="Object.keys(timetableStore.getCoursesAdded).length > 0">
       <template 
-        v-if="Object.keys(timetableStore.getCoursesAdded).length > 0"
-        v-for="course in Object.values(timetableStore.getCoursesAdded)"
+      v-for="course in Object.values(timetableStore.getCoursesAdded)"
         :key="course.courseCode"
       >
         <SelectedListItem :course="course" @handle-remove="(e) => handleRemove(course)"/>
         <q-separator spaced />
       </template>
+      <q-item-label header>
+        Total AU: 
+        <span v-if="timetableStore.getTotalAus">
+          {{ timetableStore.getTotalAus}}
+        </span>
+        <span class="text-grey-8 q-gutter-xs" v-else>
+          <q-spinner color="primary" size="1em" :thickness="1" />
+        </span>
+      </q-item-label>
+    </template>
       <template v-else>
         <q-item clickable>
           <q-item-section>
