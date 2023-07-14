25025df539cb7edb7b8a3a9c81ff30c329febf0c
Lebarnon <53988015+Lebarnon@users.noreply.github.com>
2023-07-14 16:21:22 +0800

    Update ChangeLogList.vue

--- src/components/changelogs/ChangeLogList.vue
+++ src/components/changelogs/ChangeLogList.vue
@@ -39,6 +39,11 @@ import ChangeLogItem from './ChangeLogItem.vue';
 
 const changes = [
     {
+        title: "Course details now available!",
+        dateTime: formatDateTime("2023-07-14", "16:15"),
+        description: "You can now view course details by clicking on the courses you have added on the right or by using the search bar above." 
+    },
+    {
         title: "Persisted State & Displaying of Academic Units (AU)",
         dateTime: formatDateTime("2023-07-10", "17:23"),
         description: "Your timetable data will now be saved even when you close or leave the website. Additionally, you will be able to view the number of Academic Units (AU) awarded by different modules and the total number of AU you currently have." 
@@ -52,6 +57,21 @@ const changes = [
 ]
 
 const bugfixes = [
+    {
+        title: "#3 EE2101 error",
+        dateTime: formatDateTime("2023-06-28", "16:00"),
+        description: "EE2101 is not valid error."
+    },
+    {
+        title: "#2 CY1500 and CY1308 error",
+        dateTime: formatDateTime("2023-05-31", "23:24"),
+        description: "CY1500 and CY1308 cannot be found."
+    },
+    {
+        title: "#1 E2103L and E2110L error",
+        dateTime: formatDateTime("2023-05-31", "23:24"),
+        description: "Unable to add Lab modules such as E2103L and E2110L to the planner."
+    },
 ]
 
 function formatDateTime(date, time){
