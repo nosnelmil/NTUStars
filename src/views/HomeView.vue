5e77f53bcc92a4b4eed98277b2df89d581960ba0
Lenson <53988015+Lebarnon@users.noreply.github.com>
2023-07-21 23:23:15 +0800

    Add google adsense

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,18 @@
 <template>
 <div class="subcontent">
   <div class="row justify-center">
+    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5236923194604774"
+        crossorigin="anonymous"></script>
+    <!-- Banner -->
+    <ins class="adsbygoogle"
+        style="display:block"
+        data-ad-client="ca-pub-5236923194604774"
+        data-ad-slot="4766952076"
+        data-ad-format="auto"
+        data-full-width-responsive="true"></ins>
+    <script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
     <TimetableSection />
   </div>
   <div class="row justify-center q-mt-sm">
