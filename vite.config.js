3cdaa7a1d20bbcadd7b9a4787032991a59549693
Lenson <nosnelmil@gmail.com>
2025-04-05 17:11:38 +0800

    bump packages
    Signed-off-by: Lenson <nosnelmil@gmail.com>

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,17 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { quasar, transformAssetUrls } from '@quasar/vite-plugin'
 
+import { sassMigratorQuasar } from 'rollup-plugin-sass-migrator';
+
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue({
     template: { transformAssetUrls }
   }),
   quasar({
-    sassVariables: 'src/quasar-variables.sass'
+    sassVariables: 'src/quasar-variables.sass',
+    plugins: [sassMigratorQuasar()]
   })
   ],
   resolve: {
